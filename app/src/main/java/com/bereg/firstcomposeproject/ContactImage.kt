a179927654f1c4b25a06e2cb1c4fbe27183bc96b
Panchenko Zakhar <zakhar-panchenko@yandex.ru>
2025-11-09 10:57:18 +0300

    "[feature] Обернул получение строковых данных в remember."

--- app/src/main/java/com/bereg/firstcomposeproject/ContactImage.kt
+++ app/src/main/java/com/bereg/firstcomposeproject/ContactImage.kt
@@ -7,9 +7,9 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
@@ -21,6 +21,10 @@ import coil.request.ImageRequest.Builder
 
 @Composable
 fun ContactImage(contact: Contact) {
+    val initials = remember(contact) {
+        getInitials(contact)
+    }
+
     Box(
         contentAlignment = Alignment.Center,
         modifier = Modifier
@@ -34,7 +38,7 @@ fun ContactImage(contact: Contact) {
                     .background(Color.LightGray, CircleShape)
             ) {
                 Text(
-                    text = getInitials(contact),
+                    text = initials,
                     color = Color.Black,
                     fontSize = 24.sp,
                     fontWeight = FontWeight.Bold
